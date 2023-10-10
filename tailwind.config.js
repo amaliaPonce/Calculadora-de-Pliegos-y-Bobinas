b080c22ad031b3213dc0be959a29595ecc00c9b1
Amalia <amaliaponcetoledo@gmail.com>
2023-10-10 16:20:30 +0200

    componente costos

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Montserrat', 'sans'], // Ejemplo de tipografía personalizada
+      },
+      colors: {
+        custom: {
+          primary: '#FF5733', // Ejemplo de color personalizado
+        },
+      },
+    },
+  },
+  plugins: [],
+}
